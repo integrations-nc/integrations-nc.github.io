4df33e2830b627222ea7fe37e0c4262686c9d2b3
mehamasum <mehamasum@gmail.com>
2020-03-16 20:46:37 +0600

    Updates

---
+++ precache-manifest.3bcdb9801d3706b0e5790ed38c3864fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ee0cccf9094cf94fa3a",
+    "url": "/static/js/main.5ee0cccf.chunk.js"
+  },
+  {
+    "revision": "cf7a894fa10fac078e6b",
+    "url": "/static/js/1.cf7a894f.chunk.js"
+  },
+  {
+    "revision": "5ee0cccf9094cf94fa3a",
+    "url": "/static/css/main.520491c3.chunk.css"
+  },
+  {
+    "revision": "3312d2bf5dfdbc5979f500cdef0acb8f",
+    "url": "/index.html"
+  }
+];
